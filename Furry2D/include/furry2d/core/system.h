709ab8ceed9ef6493690bd11886f0325f78eb192
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-02-27 10:40:58 +0100

    Add system class

---
+++ Furry2D/include/furry2d/core/system.h
@@ -0,0 +1,51 @@
+#ifndef __FURRY_CORE_SYSTEM_H__
+#define __FURRY_CORE_SYSTEM_H__  
+
+/*
+* ****************************************
+*
+* This file is part of Furry2D, a simple gameframework for 2D desktop games.
+*
+* Copyright (c) 2015 Furry2D. All rights reserved.
+*
+* For the full copyright and license information, please view the LICENSE.txt
+* file that was distributed with this source code.
+*
+* \Author Alexander Knueppel
+*
+* ****************************************
+*/
+
+FURRY_NS_BEGIN
+
+class Engine;
+
+// Abstract 'system' (aka module)
+class System {
+public:
+	friend class Engine;
+
+	System(std::string name);
+
+	System(const System&) = delete;
+	System& operator = (const System&) = delete;
+
+	virtual bool initialize();
+	virtual void update();
+	virtual void shutdown();
+
+	const std::string& getName() const {
+		return name_;
+	}
+
+protected:
+	void setName(std::string name);
+
+	Channel channel_;
+	Engine* engine_;
+	std::string name_;
+};
+
+FURRY_NS_END
+
+#endif
