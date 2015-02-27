6890e809d5b531321d0ccd062452d6b3e0fd562e
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-02-27 09:16:32 +0100

    Add includes (core and util headers)

--- Furry2D/include/furry2d/furry2d.h
+++ Furry2D/include/furry2d/furry2d.h
@@ -13,8 +13,33 @@
 * ****************************************
 */
 
+#include <furry2d/preprocessor.h>
+#include <sstream>
+#include <string>
+#include <map>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdexcept>
+#include <limits>
 
+using std::cout;
+using std::cerr;
+using std::endl;
 
-
+//// Util classes 
+#include <furry2d/util/concurrentqueue.h>
+#include <furry2d/util/active.h>
+// Basic subset of core classes follows here 
+#include <furry2d/core/version.h>
+// Logging
+#include <furry2d/core/loglevel.h>
+#include <furry2d/core/logger.h>
+#include <furry2d/core/logmessage.h>
+#include <furry2d/core/logsink.h>
 
 #endif /* __FURRY_FURRY2D_H__ */
