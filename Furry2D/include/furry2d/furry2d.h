824ed2bbf25bbbdf44e2abcac1fa49b78278614a
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-02-27 08:31:21 +0100

    Add main header file

---
+++ Furry2D/include/furry2d/furry2d.h
@@ -0,0 +1,20 @@
+#ifndef __FURRY_FURRY2D_H__
+#define __FURRY_FURRY2D_H__
+
+/*
+* ****************************************
+* Copyright (c) 2014 Furry Graphics/Game Engine. All rights reserved.
+*
+* For the full copyright and license information, please view the LICENSE.txt
+* file that was distributed with this source code.
+*
+* \Author Alexander Knueppel
+*
+* ****************************************
+*/
+
+
+
+
+
+#endif /* __FURRY_FURRY2D_H__ */
